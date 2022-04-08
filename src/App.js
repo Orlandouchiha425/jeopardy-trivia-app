6601788c0471a523852efad7021a4fbbec0c3afa
orlandouchiha425 <valadez425@gmail.com>
2022-04-08 11:52:49 -0400

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
 import './App.css';
+import Data from './components/data';
+import axios from 'axios';
+import { useState,useEffect } from 'react';
+
+
+
 
 function App() {
+
+const [state,setState]=useState([])
+
+  const [count, setCount]=useState(0)
+
+
+  function decreaseCount(){
+    setCount(count-100)
+  }
+
+
+function increaseCount(){
+  setCount(count+100)
+}
+
+// function setData(datas){
+//   setState(datas)
+// }
+
+
+
+useEffect(()=>{
+    
+  (async ()=>{
+try{
+    const response=await axios.get(`http://jservice.io/api/random`)
+    setState(response.data)
+}catch(err){
+    console.log(err)
+}
+  })()
+},[])
+
+
+
+          
+//  )
+
+// }
+
+
+
+
+
+
+
+
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+
+      <div>Points:</div>
+           <h2>
+             <button onClick={decreaseCount}>decrease</button>
+                  {count}
+             <button onClick={increaseCount}>increase</button>
+           </h2>
+        <div>Category:</div>
+        <div>
+        
+          <button  >
+          Get Question
+        </button>
+        </div>
+
+        {
+       state.map(element=>(
+         <div>
+           <h1>The Category is {element.category.title}</h1>
+           <h2>The question is:{element.question} </h2>   
+           <h3>The answer{element.answer}</h3>
+           <h4>Points:{element.value}</h4>
+           </div>
+       ))
+   }
+
+ 
+<Data />
+
+      
     </div>
   );
+ 
+
 }
+
+
 
 export default App;
---
+++ src/components/data.js
@@ -0,0 +1,60 @@
+// import { useState,useEffect } from "react";
+// // import {useParams} from "react-router-dom"
+// import axios from "axios";
+// import { useParams } from "react-router-dom";
+
+
+export default function Data(props){
+  return <h1>test</h1>
+}
+// const [state, setState]=useState([])
+// const {id}=useParams()
+
+// useEffect(()=>{
+    
+//   (async ()=>{
+// try{
+//     const response=await axios.get(`http://jservice.io/api/random`)
+//     setState(response.data)
+// }catch(err){
+//     console.log(err)
+// }
+//   })()
+// },[])
+
+
+//  return(
+//      <>
+//    {
+//        state.map(element=>(
+//          <div>
+//            <h1>The Category is {element.category.title}</h1>
+//            <h2>The question is:{element.question} </h2>   
+//            <h3>The answer{element.answer}</h3>
+//            <h4>Points:{element.value}</h4>
+//            </div>
+//        ))
+//    }
+//           </>
+//  )
+
+// }
+//  const {id}=useParams()
+
+// useEffect(()=>{ 
+//     (async ()=>{
+//         const response=  await fetch(`http://jservice.io/api/random`);
+//         const data=response.json();
+//         element(data)
+
+//     }) ()
+  
+     
+  
+          
+//    }, [])
+
+
+//      )
+   
+
